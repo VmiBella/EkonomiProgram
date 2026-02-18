a83bb296b5f1ca8823ad038cf96286de0f9150dc
Vivi <VmiBella@gmail.com>
2026-02-18 14:34:12 +0100

    La till meny for att lagga till nya funktioner

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(){
 	char val;
 
 	do {
-		std::cout << "n\1. Lägg till transaktion\n2. Visa alla transaktioner\n3. Visa saldo\n4. Avsluta\n";
+		std::cout << "\n1. Lägg till transaktion\n2. Visa alla transaktioner\n3. Visa saldo\n4. Avsluta\n";
 		std::cout << "Välj ett alternativ: ";
 		std::cin >> val;
 		std::cin.ignore(); //rensa neewline
@@ -44,28 +44,14 @@ int main(){
 		else if (val == '3') {
 			double saldo = 0;
 			for (auto &t : transaktioner)
-				saldo += (typ.t == "Inkomst") ? t.belopp : -t.belopp;
+				saldo += (t.typ == "Inkomst") ? t.belopp : -t.belopp;
 			std::cout << "\nSaldo: " << saldo << " kr\n";
 		}
+	}
 		while (val!= '4');
 
 		std::cout << "Hejdå!\n";
 		return 0;
-	}
-
-	transaktioner.push_back(Transaktion("2025-02-18", "Inkomst", "Lön", 25000));
-	transaktioner.push_back(Transaktion("2025-02-19", "Utgift", "Bensin", 700));
-
-	std::cout << "Alla transaktioner:\n";
-	for (auto &t : transaktioner) {
-	t.visa();
-	}
-
-	double saldo = 0;
-	for (auto &t : transaktioner) {
-	saldo += (t.typ == "Inkomst") ? t.belopp : -t.belopp;
-	}
-	std::cout << "\nSaldo: " << saldo << "kr\n";
-
-	return 0;
+	
 }
+	
